8162733e8919e8556309db9eb60a7da5768def4b
Akul Srivastava <akulsr0@gmail.com>
2021-10-27 01:03:55 +0530

    docs: updated installation section

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # SOFIT: StackOverFlow IT!
 
 Get solutions to errors/issues within your terminal.
+
+## Installation
+
+```zsh
+pip install sofit
+
+or
+
+pip3 install sofit
+```
--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import find_packages, setup
 
 
-def read_requirements():
-    with open('requirements.txt') as req:
-        content = req.read()
-        requirements = content.split('\n')
-    return requirements
-
-
 setup(
     name='sofit',
     version='0.1',
     description='Get solutions to errors/issues within your terminal.',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=read_requirements(),
     entry_points='''
         [console_scripts]
         sofit=sofit.sofit:main
